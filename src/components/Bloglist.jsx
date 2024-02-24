21645109707d8fc1f361a336fd6c9fa8b73e1269
marufmustarmoon <marufmustarmoon@gmail.com>
2024-02-24 17:24:08 +0600

    alt text removed

--- src/components/Bloglist.jsx
+++ src/components/Bloglist.jsx
@@ -102,7 +102,7 @@ function BlogList({ searchTerm }) {
       <div className="grid grid-cols-1 gap-y-10 gap-x-8  mt-8 shadow-sm rounded-lg bg-white ">
         {blogs.map(blog => (
           <div key={blog.id} className="flex flex-col md:flex-row rounded-xl md:space-x-8 bg-stone-100 shadow-gray-400">
-            <img className="w-[20rem] rounded-lg bg-gray-300" src={`${API_URL}${blog.banner}` || 'https://img.freepik.com/free-photo/teamwork-making-online-blog_53876-94868.jpg?w=740&t=st=1708758855~exp=1708759455~hmac=fac13dc445abed1246ffd83debfee0b086a5369327499a4818d65d227f22ad36'}  alt="" />
+            <img className="w-[20rem] h-[15rem] rounded-lg bg-gray-300" src={`${API_URL}${blog.banner}` || 'https://img.freepik.com/free-photo/teamwork-making-online-blog_53876-94868.jpg?w=740&t=st=1708758855~exp=1708759455~hmac=fac13dc445abed1246ffd83debfee0b086a5369327499a4818d65d227f22ad36'}  alt="" />
              
             <div>
               <p>total views: {Math.ceil(blog.total_views / 2)}</p>
