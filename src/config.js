c90b243cf418ca136fa4951b70a149e3e23039f9
marufmustarmoon <marufmustarmoon@gmail.com>
2024-02-24 16:34:43 +0600

    api url changed

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// const API_URL = "https://qtec-solution-task-1.onrender.com";
-const API_URL =  "http://127.0.0.1:8000"
+const API_URL = "https://qtec-solution-task-1.onrender.com";
+// const API_URL =  "http://127.0.0.1:8000"
 export default API_URL
